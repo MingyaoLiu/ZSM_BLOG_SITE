64918365389409fc0fe1df8633a3ebe60c360e39
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-04-16 00:42:27 -0400

    add libs
    Signed-off-by: zodiacsoulmate <zodiacsoulmate@gmail.com>

---
+++ ZSM_public/ZSM_javascript/ZSM_route/BlogAppRoute.js
@@ -0,0 +1,25 @@
+angular.module('appRoutes', []).config(['$routeSegmentProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		// home page
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'MainController'
+		})
+
+		.when('/tablets', {
+			templateUrl: 'views/tablet.html',
+			controller: 'TabletController'
+		})
+
+		.when('/phones', {
+			templateUrl: 'views/phone.html',
+			controller: 'PhoneController'	
+		});
+
+	$locationProvider.html5Mode(true);
+
+}]);
+
+
