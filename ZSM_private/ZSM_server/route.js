35c25ddd681349b48af17051f3d80bbe48971fa4
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-04-17 03:22:14 -0400

    update to express 4.x routing and adding testing templates

--- ZSM_private/ZSM_server/route.js
+++ ZSM_private/ZSM_server/route.js
@@ -1,4 +1,4 @@
-module.exports = function(app) {
+module.exports = express.Router() {
 
 	app.get('*', function(req, res) {
 		res.sendfile('/ZSM_public/index.html'); // load our public/views/index.html file
