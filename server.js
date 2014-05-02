d79c8a5f0cddf55f91e5803e04387fa40c18e3ec
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-05-02 18:38:46 -0400

    only a little bit changed

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ var app = express();
 var port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/ZSM_public'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.multipart());
+
 app.listen(port);
 console.log('TheMatrixBlog is running on port ' + port);
 exports = module.exports = app;
