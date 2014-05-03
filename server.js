e9d98629b8cb203cfa16c5c70d90f39f70d5f0a6
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-05-02 23:17:53 -0400

    seperating routing, add express module

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ var less = require('less');
 var mongojs = require('mongojs');
 var db = mongojs('107.182.179.168/blogdb', ['firstblog']);
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
 var port = process.env.PORT || 8000;
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); 				// get an instance of the express Router
+
+
+
+
+
+
 
 app.use(express.static(__dirname + '/ZSM_public'));
 
@@ -12,8 +25,10 @@ app.listen(port);
 console.log('TheMatrixBlog is running on port ' + port);
 
 db.runCommand({ping:1}, function(err, res) {
-    if(!err && res.ok) console.log("we're up");
+    if(!err && res.ok) console.log("Connected to mongodb @ blogdb @ 'firstblog'");
 });
-
+db.firstblog.runCommand('count', function(err, res) {
+    console.log(res);
+});
 
 exports = module.exports = app;
