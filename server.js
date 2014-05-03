5a1307689bfafd3d4f9159203502995d7314dd1d
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-05-03 18:45:43 -0400

    no route.js anymore, not working

--- server.js
+++ server.js
@@ -4,31 +4,37 @@ var less = require('less');
 var mongojs = require('mongojs');
 var db = mongojs('107.182.179.168/blogdb', ['firstblog']);
 var app = express();
+var router = express.Router(); 
 var bodyParser = require('body-parser');
-
-app.use(bodyParser());
 var port = process.env.PORT || 8000;
+app.use(bodyParser());
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router(); 				// get an instance of the express Router
+router.use(function(req, res, next) {
+	console.log('Something is happening.');
+	next();
+});
 
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
 
+// more routes for our API will happen here
 
-
-
-
+app.use('/api', router);
 
 app.use(express.static(__dirname + '/ZSM_public'));
+app.get('/', function(req, res) {
+	res.sendfile('index.html');
+});
 
 app.listen(port);
-console.log('TheMatrixBlog is running on port ' + port);
+console.log('WEB is running @ port ' + port);
 
 db.runCommand({ping:1}, function(err, res) {
-    if(!err && res.ok) console.log("Connected to mongodb @ blogdb @ 'firstblog'");
+	if(!err && res.ok) console.log("DB connected @ blogdb @ 'firstblog'");
 });
-db.firstblog.runCommand('count', function(err, res) {
-    console.log(res);
-});
+//db.firstblog.runCommand('count', function(err, res) {
+//    console.log(res);
+//});
 
 exports = module.exports = app;
